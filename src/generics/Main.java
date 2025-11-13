1fcf3ca6a22f924340c816051ebba61229629024
Yash Panchal <panchalyash089@gmail.com>
2025-11-13 13:53:48 +0530

    Bounded type parameters

--- src/generics/Main.java
+++ src/generics/Main.java
@@ -2,9 +2,13 @@ package generics;
 
 public class Main {
     public static void main(String[] args) {
-        Pair<String, Integer> pair = new Pair<>("Age", 30);
-        System.out.println(pair.getKey());
-        System.out.println(pair.getValue());
+          //Generic Interface
+//        Pair<String, Integer> pair = new Pair<>("Age", 30);
+//        System.out.println(pair.getKey());
+//        System.out.println(pair.getValue());
+
+        //Bounded Type Parameter
+        Box<Integer> box = new Box<>();
 
     }
 }
